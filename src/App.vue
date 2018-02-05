ce80811f81927b1ec9c4b6ca0cbef697164e6f73
Geison Santos <geison.sn@gmail.com>
2018-02-05 19:15:26 -0400

    Estruturação do conteúdo das imagens em painéis

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
   <h1 class="centralizado">{{titulo}}</h1>
   <ul class="lista-fotos">
     <li class="lista-fotos-item" v-for="foto of fotos">
-      <img :src="foto.url" :title="foto.title"/>
+      <div class="painel">
+        <h2 class="painel-titulo">{{foto.titulo}}</h2>
+        <div class="painel-corpo">
+          <img class="imagem-responsiva" :src="foto.url" :title="foto.title">
+        </div>
+      </div>
     </li>
   </ul>
   
@@ -40,5 +45,29 @@ export default {
 }
 .lista-fotos .lista-fotos-item {
   display: inline-block;
+}
+.imagem-responsiva {
+  width: 100%;
+}
+
+/* estilo do painel */ 
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
 }
 </style>
