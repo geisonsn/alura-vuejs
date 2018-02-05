a65cfa2a1595a8f41c3076d1ecc52ffaa355e189
Geison Santos <geison.sn@gmail.com>
2018-02-05 01:35:03 -0400

    Iterando com v-for

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
 <div>
   <h1>{{ titulo }}</h1>
-  <img v-bind:src="foto.url" :alt="foto.title"/>
+  <ul>
+    <li v-for="foto of fotos">
+      <img :src="foto.url" :title="foto.title"/>
+    </li>
+  </ul>
+  
 </div>
 </template>
 
@@ -10,10 +15,16 @@ export default {
   data() {
     return {
       titulo: "Alurapic",
-      foto: {
-        url: "http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg",
-        title: "cachorro"
-      }
+      fotos: [
+        {
+          url: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
+          title: "Cachorro"
+        },
+        {
+          url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOhmlmzV4-Sifx5BIc2SXeA-1CtZJf8jb8V_vPZyKbXIQJKU-rkxGO6OM",
+          title: "Gato"
+        },
+      ]
     };
   }
 }
