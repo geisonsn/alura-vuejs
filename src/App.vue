26531b9ee25f019244b356de906405a6e68eff49
Geison Santos <geison.sn@gmail.com>
2018-02-08 14:37:24 -0400

    Converte a imagem no componente ImagemResponsiva

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
   <ul class="lista-fotos">
     <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
       <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :title="foto.titulo">
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
       </meu-painel>
     </li>
   </ul>
@@ -17,9 +17,11 @@
 
 <script>
 import Painel from './components/shared/painel/Painel.vue';
+import ImagemResponsiva from './components/shared/imagem-responsiva/ImagemResponsiva.vue';
 export default {
   components: {
-    'meu-painel': Painel
+    'meu-painel': Painel,
+    'imagem-responsiva': ImagemResponsiva
   },
   data() {
     return {
