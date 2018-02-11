841e18bdef28455cf79368bdec049441e2bfe9d1
Geison Santos <geison.sn@gmail.com>
2018-02-11 19:48:49 -0400

    Usando evento customizado

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -8,7 +8,7 @@
     <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
       <meu-painel :titulo="foto.titulo">
           <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-          <meu-botao rotulo="remover" tipo="button" @click.native="remove(foto)"/>
+          <meu-botao rotulo="remover" tipo="button" @botaoAtivado="remove(foto)"/>
       </meu-painel>
     </li>
   </ul>
@@ -29,9 +29,7 @@ export default {
   },
   methods: {
     remove(foto) {
-      if (confirm('Confirma?')) {
-        alert('Remover foto ' + foto.titulo);
-      }
+      alert('Remover foto ' + foto.titulo);
     }
   },
   data() {
