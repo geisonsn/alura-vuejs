14a27a63c97d2df5d71a539fda4095775b2b16b2
Geison Santos <geison.sn@gmail.com>
2018-02-22 03:10:55 -0400

    Criação da diretiva para rotacionar elementos

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -7,7 +7,7 @@
   <ul class="lista-fotos">
     <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
       <meu-painel :titulo="foto.titulo">
-          <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+          <imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform/>
           <meu-botao 
             rotulo="remover" 
             tipo="button" 
---
+++ src/directives/Transform.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+
+Vue.directive('meu-transform', {
+  bind(el, binding, vnode) {
+    let current = 0;
+    el.addEventListener('dblclick', function() {
+      current+=90;
+      this.style.transform = `rotate(${current}deg)`;
+    });
+  }
+});
